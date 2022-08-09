6a826264749ca2602802d1d2bc873d6faf97af8c
gooteen <108371872+gooteen@users.noreply.github.com>
2022-08-09 13:18:05 +0300

    added a cursor

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+    private PlayerInputActions _input;
+    public static InputManager Instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance != null & !_instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        _input = new PlayerInputActions();
+        _input.PlayerMovement.Enable();
+        _input.Mouse.Enable();
+    }
+
+    public Vector2 GetMovementDirection()
+    {
+        return _input.PlayerMovement.Run.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMousePosition()
+    {
+        return _input.Mouse.MousePosition.ReadValue<Vector2>();
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,27 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
     [SerializeField] private PlayerAnimation anim;
     private Rigidbody2D _rb;
-    private PlayerInputActions _input;
 
     void Start()
     {
         Debug.Log(Mathf.FloorToInt(0.8f));
-        _input = new PlayerInputActions();
-        _input.PlayerMovement.Enable();
         _rb = GetComponent<Rigidbody2D>();
         //input.PlayerMovement.Run.performed += Move;
     }
     
     void FixedUpdate()
     {
-        Vector2 direction = _input.PlayerMovement.Run.ReadValue<Vector2>();
+        Vector2 direction = InputManager.Instance.GetMovementDirection();
         Move(direction);
         anim.SetDirection(direction);
     }
