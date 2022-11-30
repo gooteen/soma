f436b7436b19e5ad18fa552800948f1036620393
gooteen <108371872+gooteen@users.noreply.github.com>
2022-11-30 13:54:16 +0300

    experimented with multi-level tiles

---
+++ Assets/Scripts/General/TileSortingSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSortingSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject _elevationTilemap;
+    private TilemapRenderer _renderer;
+
+    void Start()
+    {
+        _renderer = _elevationTilemap.GetComponent<TilemapRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Enter");
+        _renderer.sortingOrder = 1;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        _renderer.sortingOrder = 2;
+    }
+}
