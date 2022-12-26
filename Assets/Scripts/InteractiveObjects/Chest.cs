e26ed854d7ee961dbbcb1829187a73aed6237f9c
gooteen <108371872+gooteen@users.noreply.github.com>
2022-12-26 22:35:01 +0300

    added chests

---
+++ Assets/Scripts/InteractiveObjects/Chest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : InteractiveObject
+{
+    [SerializeField] private Slot[] _items;
+
+    internal override void Start()
+    {
+        _interactive = true;
+    }
+
+    internal override void OnInteract()
+    {
+        if (_interactive)
+        {
+            Debug.Log("Heeey!");
+            _interactive = false;
+            foreach (Slot item in _items)
+            {
+                Engine.Instance.AddItemToInventory(item._itemId, item._quantity);
+            }
+        } 
+    }
+}
