5ac52643093ee6edbd28e9335bb0700656b4bc5e
gooteen <108371872+gooteen@users.noreply.github.com>
2023-02-05 17:38:04 +0300

    added item sprinkles

---
+++ Assets/Scripts/Items/Visual Pickup System/ItemSprinkler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSprinkler : MonoBehaviour
+{
+    [SerializeField] private GameObject _poppedItemPrefab;
+    [SerializeField] private Transform _spawningPoint;
+
+    [SerializeField] private float _pauseBetweenThrows;
+    [SerializeField] private float _forceToSprinkleWith;
+
+    private Slot[] _itemsToSprinkle;
+
+    void Start()
+    {
+        _itemsToSprinkle = null;
+
+        //TEMP
+        StartSpawningItems(new Slot[] { new Slot(ItemID.Wheat, 1), new Slot(ItemID.Apple, 1) });
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartSpawningItems(Slot[] items)
+    {
+        _itemsToSprinkle = items;
+        StartCoroutine("Sprinkle");
+    }
+
+    private IEnumerator Sprinkle()
+    {
+        for (int i = 0; i < _itemsToSprinkle.Length; i++)
+        {
+            GameObject _newPoppedItemObject = Instantiate(_poppedItemPrefab);
+
+            _newPoppedItemObject.transform.position = new Vector3(_spawningPoint.position.x, _spawningPoint.position.y, _newPoppedItemObject.transform.position.z);
+
+            PoppedItem _newPoppedItem = _newPoppedItemObject.GetComponent<PoppedItem>();
+
+            Item _item = Engine.Instance.GetItemByID(_itemsToSprinkle[i]._itemId);
+            _newPoppedItem.InitializePoppedItem(_item.GetItemIcon(), _itemsToSprinkle[i]);
+            Vector2 _throwDirection;
+
+            if (i % 2 == 0)
+            {
+                _throwDirection = new Vector2(-0.5f, 1);
+            } else
+            {
+                _throwDirection = new Vector2(0.5f, 1);
+            }
+
+            _newPoppedItem.Throw(_throwDirection, _forceToSprinkleWith);
+
+            yield return new WaitForSeconds(_pauseBetweenThrows);
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Visual Pickup System/PoppedItem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoppedItem : MonoBehaviour
+{
+    [SerializeField] private float _delaySeconds;
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _renderer;
+    private Slot _itemSlot;
+    private bool _isPulledToPlayer;
+
+    void Awake()
+    {
+        _isPulledToPlayer = false;
+        _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (_isPulledToPlayer)
+        {
+            Debug.Log("pulling!");
+            Debug.DrawLine(transform.position, Engine.Instance.Player.transform.position);
+            //transform.Translate((Engine.Instance.Player.transform.position - transform.position).normalized * Time.deltaTime * _speed);
+            _rb.AddForce((Engine.Instance.Player.transform.position - transform.position).normalized * _speed, ForceMode2D.Force);
+        }
+    }
+    
+    public void InitializePoppedItem(Sprite itemImage, Slot itemSlot)
+    {
+        _renderer.sprite = itemImage;
+        _itemSlot = itemSlot;
+        StartCoroutine("Delay");
+    }
+
+    public void Throw(Vector2 _dir, float _force)
+    {
+        _rb.AddForce(_dir.normalized * _force, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(_delaySeconds);
+        _isPulledToPlayer = true;
+        _rb.gravityScale = 0;
+        //_rb.velocity = new Vector2(0,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Engine.Instance.AddItemToInventory(_itemSlot._itemId, _itemSlot._quantity);
+        Destroy(gameObject);
+    }
+}
