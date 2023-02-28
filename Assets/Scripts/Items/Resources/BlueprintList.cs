f89beda1d6fa79081c35be2fd84303df412a3637
gooteen <108371872+gooteen@users.noreply.github.com>
2023-02-28 20:55:18 +0300

    add ui for the crafting system

---
+++ Assets/Scripts/Items/Resources/BlueprintList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Blueprint
+{
+    public ItemID _itemToBeCrafted;
+}
+
+[CreateAssetMenu(fileName = "BlueprintList", menuName = "BlueprintList")]
+public class BlueprintList : ScriptableObject
+{
+    
+}
